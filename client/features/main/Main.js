37d9ce84103a43d6dfa270d549b3859edf4ccfce
patKilcullen <patrickjkilcullen@gmail.com>
2023-01-20 17:03:06 -0600

    get all words + get all  defs + randomize real def

--- client/features/main/Main.js
+++ client/features/main/Main.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux'
 
 
 
-import { selectWord, getWord, selectDefinition, getDefinition, getMeriDefinition, getMeri,getFakeWords, selectFakeWords } from './mainSlice'
+import { selectWord, getWord, selectDefinition, getDefinition,getFakeWords, selectFakeWords, getFakeDefinitions, selectFakeDefinitions, clearFakeDefs } from './mainSlice'
 
 
 
@@ -13,65 +13,122 @@ const dispatch = useDispatch()
 
 
 
-// useEffect(()=>{
-//     dispatch(getWord())
-//     // .then(()=>{
-//     //     dispatch(getDefinition())
-//     // })
-//     // dispatch(getDefinition('ass'))
-// },[])
-
 const word = useSelector(selectWord)
-
 const definition = useSelector(selectDefinition)
+
+
 // console.log("DEFINTTT: ", definition)
 
 
-const handleGetMeri = ()=>{
-  dispatch(getMeriDefinition(word[0]))
+
+const handleGetDefs = ()=>{
+  dispatch(getDefinition(word[0]))
+  .then(()=>{
+    handleGetFakeDefinitions()
+  })
+  .then(()=>{
+    pushDefs()
+  })
+  
 }
 
-const handleGetWord = ()=>{
-  dispatch(getWord())
+const pushDefs = ()=>{
+  for(const def of fakeDefinitions){
+    allDefs.push(def)
+  }
+  allDefs.push(definition)
+  console.log("ALLL DEFSS INSIDE: ", allDefs)
 }
-const fakeDefs = [];
-// const fakeWords = []
-const handleGetFakes = ()=>{
+console.log("ALLL DEFSS: ", allDefs)
+
+useEffect(()=>{
+},[handleGetFakeDefinitions])
+
+const handleGetWord = ()=>{
+  dispatch(getWord()).then(()=>{
+    handleGetFakeWords()
+  })
+}
+
+const handleGetFakeWords = ()=>{
 let count = 0
 while (count < 5){
   dispatch(getFakeWords())
-  // .then(()=>{
-  //   let fakeWord = useSelector(selectWord).then(()=>{
-  //     fakeWords.push(fakeWord)
-  //   })
-   
-  // })
-  
-    // dispatch(getMeriDefinition(word[0]))
-    // let fakeDef = useSelector(selectDefinition)
-    // console.log("FAKE DEF: ", fakeDef)
-    // fakeDefs.push(fakeDef)
- 
   count++
 }
 }
-
-
 const fakeWords = useSelector(selectFakeWords)
-console.log("FAKE DEFS", fakeDefs)
 console.log("FAKE Words", fakeWords)
+
+
+
+const handleGetFakeDefinitions = ()=>{
+  fakeWords.forEach(word => {
+    dispatch(getFakeDefinitions(word))
+  }).then(()=>{
+    getFakeDefinitions(word[0])
+  })
+
+}
+
+const fakeDefinitions = useSelector(selectFakeDefinitions)
+console.log("FAKE DEFS: ", fakeDefinitions )
+
+
+
+function shuffleArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+
+
+console.log("DEF: ", definition)
+let allDefs;
+const play = ()=>{
+let allDefs = Object.values(fakeDefinitions)
+console.log("ALL DERFTS LENGH: ", allDefs.length)
+let radnomNum = Math.floor(Math.random()* allDefs.length)
+console.log("NUM : ", radnomNum)
+allDefs.splice(radnomNum,0, definition)
+console.log("ALLL DA DEFS : ", allDefs)
+}
+
 
   return (
     <div>
     <div>Let's BALDERDASH!!!</div>
     <button onClick={()=> handleGetWord()}>Get Word</button>
 <h2>{typeof word[0] === 'string' ? word[0] : " "}</h2>
-<button onClick={()=> handleGetMeri()}>Get Definition</button>
+<button onClick={()=> handleGetDefs()}>Get Definition</button>
 
 <h3>{typeof definition === 'string' ? definition : " "}</h3>
-<button onClick={()=> handleGetMeri()}>Try For Another definition</button>
-<button onClick={()=> handleGetFakes()}>Get Fake Definitions</button>
-
+<button onClick={()=> handleGetDefs()}>Try For Another definition</button>
+<button onClick={()=> handleGetFakeWords()}>Get Fake Words</button>
+<button onClick={()=> handleGetFakeDefinitions()}>Get Fake Definitions</button>
+<button onClick={()=> play()}>Play</button>
     </div>
   )
 }
--- client/features/main/mainSlice.js
+++ client/features/main/mainSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import defaultDefs from './defaultFakeDefs';
 // require('dotenv').config();
 // const jwt = require('jsonwebtoken')
 
@@ -42,24 +43,24 @@ export const getFakeWords = createAsyncThunk(
     }
   }
 );
+//   export const getDefinition = createAsyncThunk(
+//     '/getDefinition',
+//     async (word) => {
+//       try {
+//         const { data } = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+// //    console.log("DATA : ", data[0].word)
+// //    console.log("DATA2 : ", data[0].meanings[0].definitions[1].definition)
+// //    console.log("DEF LENGTH: ", data[0].meanings[0].definitions.length)
+//    const numOfDefs = Math.floor(Math.random() * data[0].meanings[0].definitions.length)
+//    console.log("NUM OF DEFS : ", numOfDefs)
+//         return data[0].meanings[0].definitions[numOfDefs].definition
+//       } catch (error) {
+//         console.log("EROR IN GET DEFINITIOM")
+//         return error.message;
+//       }
+//     }
+//   );
   export const getDefinition = createAsyncThunk(
-    '/getDefinition',
-    async (word) => {
-      try {
-        const { data } = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
-//    console.log("DATA : ", data[0].word)
-//    console.log("DATA2 : ", data[0].meanings[0].definitions[1].definition)
-//    console.log("DEF LENGTH: ", data[0].meanings[0].definitions.length)
-   const numOfDefs = Math.floor(Math.random() * data[0].meanings[0].definitions.length)
-   console.log("NUM OF DEFS : ", numOfDefs)
-        return data[0].meanings[0].definitions[numOfDefs].definition
-      } catch (error) {
-        console.log("EROR IN GET DEFINITIOM")
-        return error.message;
-      }
-    }
-  );
-  export const getMeriDefinition = createAsyncThunk(
     '/getDefinition',
     async (word) => {
       console.log("WORD IN GET MERI: ", word)
@@ -81,22 +82,50 @@ export const getFakeWords = createAsyncThunk(
       }
     }
   );
-  export const getMeri = createAsyncThunk(
-    '/getDefinition',
+
+  export const getFakeDefinitions = createAsyncThunk(
+    '/getFakeDefinitions',
     async (word) => {
       console.log("WORD IN GET MERI: ", word)
       try {
-        const data = await axios.get(`/api/dictionary/${word}`)
         // const { data } = await axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.dictionaryKey}`);
-        //  const { data } = await axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=3ca1a8b0-158e-4e88-b635-579bf43719f4`);
-        console.log("DATA getMEri: ", data)
-  
+        const { data } = await axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=3ca1a8b0-158e-4e88-b635-579bf43719f4`);
+         console.log("DATA in reg meri: ", data)
+         console.log("destructed in  reg meri: ", data[0].shortdef)
+         if(data[0].shortdef){
+         const numOfDefs = Math.floor((Math.random() * data[0].shortdef.length))
+         console.log("DEF NUMBER: ", numOfDefs)
+         return data[0].shortdef[numOfDefs]
+         }
+         else{
+          const defaultNum = Math.floor((Math.random() * defaultDefs.length))
+
+          return defaultDefs[defaultNum]
+         }
       } catch (error) {
         console.log("EROR IN Get MERi DEFINITIOM")
         return error.message;
       }
     }
   );
+
+
+  // export const getMeri = createAsyncThunk(
+  //   '/getDefinition',
+  //   async (word) => {
+  //     console.log("WORD IN GET MERI: ", word)
+  //     try {
+  //       const data = await axios.get(`/api/dictionary/${word}`)
+  //       // const { data } = await axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.dictionaryKey}`);
+  //       //  const { data } = await axios.get(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=3ca1a8b0-158e-4e88-b635-579bf43719f4`);
+  //       console.log("DATA getMEri: ", data)
+  
+  //     } catch (error) {
+  //       console.log("EROR IN Get MERi DEFINITIOM")
+  //       return error.message;
+  //     }
+  //   }
+  // );
 
 
 const initialState = {
@@ -108,21 +137,31 @@ const initialState = {
 const mainSlice = createSlice({
     name:"main",
     initialState,
-    reducers: {},
+    reducers: {
+      clearFakeDefs(state, action){
+        state.fakeDefinitions = []
+      }
+    },
     extraReducers: (builder)=>{
         builder
         .addCase('/getWord/fulfilled', (state, action) => {
             console.log("ACtion Paylod ", action.payload)
            state.word = action.payload
+          //  state.fakeWords.push(action.payload)
         })
         .addCase('/getDefinition/fulfilled', (state, action) => {
             // console.log("ACtion Paylod ", action.payload)
            state.definition = action.payload
+          //  state.fakeDefinitions.push(action.payload)
         })
         .addCase('/getFakeWords/fulfilled', (state, action) => {
           // console.log("ACtion Paylod ", action.payload)
          state.fakeWords.push(action.payload)
       })
+      .addCase('/getFakeDefinitions/fulfilled', (state, action) => {
+         console.log("ACtion Paylod ", action.payload)
+       state.fakeDefinitions.push(action.payload)
+    })
     }
 })
 
@@ -133,10 +172,11 @@ export const selectDefinition = (state)=>{
     return state.main.definition
 }
 export const selectFakeDefinitions = (state)=>{
-    return state.main.definitions
+    return state.main.fakeDefinitions
 }
 export const selectFakeWords = (state)=>{
   return state.main.fakeWords
 }
+export const { clearFakeDefs } = mainSlice.actions;
 
 export default mainSlice.reducer
