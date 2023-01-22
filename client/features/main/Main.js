29a75d5a8375040c04ede15de415950f377b8c9f
patKilcullen <patrickjkilcullen@gmail.com>
2023-01-21 23:21:54 -0600

    display all definitions. Increase score if right def chosen. Clears all defs.  Need to get rid of superflous code for clearing defs

--- client/features/main/Main.js
+++ client/features/main/Main.js
@@ -1,136 +1,134 @@
-import React, {useEffect} from 'react'
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-
-
-import { selectWord, getWord, selectDefinition, getDefinition,getFakeWords, selectFakeWords, getFakeDefinitions, selectFakeDefinitions, clearFakeDefs } from './mainSlice'
-
-
+import {
+  selectWord,
+  getWord,
+  selectDefinition,
+  getDefinition,
+  getFakeWords,
+  selectFakeWords,
+  getFakeDefinitions,
+  selectFakeDefinitions,
+  clearFakeDefs,
+} from "./mainSlice";
 
 const Main = () => {
-const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const word = useSelector(selectWord);
+  const definition = useSelector(selectDefinition);
+  const [defArray, setDefArray] = useState([]);
+  const [score, setScore] = useState(0);
+  const [round, setRound] = useState(0)
+  const [reply, setReply] = useState("")
+  // console.log("DEFINTTT: ", definition)
+
+  const handleGetDefs = () => {
     
+    dispatch(getDefinition(word[0]))
+      .then(() => {
+        handleGetFakeDefinitions();
+      })
+      .then(() => {
+        pushDefs();
+      })
+  };
 
+  const pushDefs = () => {
+    for (const def of fakeDefinitions) {
+      allDefs.push(def);
+    }
+    allDefs.push(definition);
+    if(definition === "That word is too weird, try again"){
+      setReply("That word is too weird, try again")
+    }
+  };
 
+  useEffect(() => {}, [handleGetFakeDefinitions]);
 
-const word = useSelector(selectWord)
-const definition = useSelector(selectDefinition)
+  const handleGetWord = () => {
+    dispatch(getWord()).then(() => {
+      handleGetFakeWords();
+    });
+  };
 
+  const handleGetFakeWords = () => {
+    dispatch(clearFakeDefs())
+     allDefs = []
+    let count = 0;
+    while (count < 5) {
+      dispatch(getFakeWords());
+      count++;
+    }
+  };
+  const fakeWords = useSelector(selectFakeWords);
 
-// console.log("DEFINTTT: ", definition)
+  const handleGetFakeDefinitions = () => {
+    fakeWords
+      .forEach((word) => {
+        dispatch(getFakeDefinitions(word));
+      })
+      .then(() => {
+        getFakeDefinitions(word[0]);
+      });
+  };
 
+  const fakeDefinitions = useSelector(selectFakeDefinitions);
 
-
-const handleGetDefs = ()=>{
-  dispatch(getDefinition(word[0]))
-  .then(()=>{
-    handleGetFakeDefinitions()
-  })
-  .then(()=>{
-    pushDefs()
-  })
   
-}
+  let allDefs;
+  let finalArr = (finalArr = Object.values(defArray));
+  const play = () => {
+    let allDefs = Object.values(fakeDefinitions);
 
-const pushDefs = ()=>{
-  for(const def of fakeDefinitions){
-    allDefs.push(def)
+    let radnomNum = Math.floor(Math.random() * allDefs.length);
+
+    allDefs.splice(radnomNum, 0, definition);
+    setDefArray(allDefs);
+    dispatch(clearFakeDefs())
+  };
+
+  const handleChooseWord = (def) => {
+setDefArray([])
+allDefs = []
+setRound(round + 1)
+setReply("CORRECT!")
+def === definition ? setScore(score + 1)
+   :
+setReply(`Wrongo! the anser was ${definition}`)
+
   }
-  allDefs.push(definition)
-  console.log("ALLL DEFSS INSIDE: ", allDefs)
-}
-console.log("ALLL DEFSS: ", allDefs)
-
-useEffect(()=>{
-},[handleGetFakeDefinitions])
-
-const handleGetWord = ()=>{
-  dispatch(getWord()).then(()=>{
-    handleGetFakeWords()
-  })
-}
-
-const handleGetFakeWords = ()=>{
-let count = 0
-while (count < 5){
-  dispatch(getFakeWords())
-  count++
-}
-}
-const fakeWords = useSelector(selectFakeWords)
-console.log("FAKE Words", fakeWords)
-
-
-
-const handleGetFakeDefinitions = ()=>{
-  fakeWords.forEach(word => {
-    dispatch(getFakeDefinitions(word))
-  }).then(()=>{
-    getFakeDefinitions(word[0])
-  })
-
-}
-
-const fakeDefinitions = useSelector(selectFakeDefinitions)
-console.log("FAKE DEFS: ", fakeDefinitions )
-
-
-
-function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-  }
-}
-
-function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-}
-
-
-
-console.log("DEF: ", definition)
-let allDefs;
-const play = ()=>{
-let allDefs = Object.values(fakeDefinitions)
-console.log("ALL DERFTS LENGH: ", allDefs.length)
-let radnomNum = Math.floor(Math.random()* allDefs.length)
-console.log("NUM : ", radnomNum)
-allDefs.splice(radnomNum,0, definition)
-console.log("ALLL DA DEFS : ", allDefs)
-}
-
-
   return (
     <div>
-    <div>Let's BALDERDASH!!!</div>
-    <button onClick={()=> handleGetWord()}>Get Word</button>
-<h2>{typeof word[0] === 'string' ? word[0] : " "}</h2>
-<button onClick={()=> handleGetDefs()}>Get Definition</button>
+      <div>Let's BALDERDASH!!!</div>
+      <h1>Score: {score}/{round}</h1>
+      <h1>{reply}</h1>
+      <button onClick={() => handleGetWord()}>Get Word</button>
+      <h2>{typeof word[0] === "string" ? word[0] : " "}</h2>
+      <button onClick={() => handleGetDefs()}>Get Definition</button>
 
-<h3>{typeof definition === 'string' ? definition : " "}</h3>
-<button onClick={()=> handleGetDefs()}>Try For Another definition</button>
+      <h3>{typeof definition === "string" ? definition : " "}</h3>
+      {/* <button onClick={()=> handleGetDefs()}>Try For Another definition</button>
 <button onClick={()=> handleGetFakeWords()}>Get Fake Words</button>
-<button onClick={()=> handleGetFakeDefinitions()}>Get Fake Definitions</button>
-<button onClick={()=> play()}>Play</button>
+<button onClick={()=> handleGetFakeDefinitions()}>Get Fake Definitions</button> */}
+      <button onClick={() => play()}>Play</button>
+      <div>
+        {/* {allDefs ? 
+allDefs.map((def)=>{
+  <h1>{55}</h1>
+}): ""
+} */}
+        {/* <h1>{defArray}</h1> */}
+      </div>
+      <div>
+        {finalArr.map((def) => {
+          return <button onClick={() => handleChooseWord(def)}>{def}</button>;
+        })}
+      </div>
+      <button
+      onClick={()=> dispatch(clearFakeDefs())}>CLEAR DEFS </button>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
