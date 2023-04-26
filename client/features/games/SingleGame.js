d94cfcc3c9090a54744b27bbd418fca6e5b762f2
patKilcullen <patrickjkilcullen@gmail.com>
2023-04-25 23:58:07 -0500

    owner can't remove self from game

--- client/features/games/SingleGame.js
+++ client/features/games/SingleGame.js
@@ -3,42 +3,50 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 
 import { selectSingleGame, fetchSingleGame } from "./singleGameSlice";
-import { fetchAllScores, selectAllScores, fetchAllGameScores, editScore, deleteScore } from "../scores/scoresSlice";
+import {
+  fetchAllScores,
+  selectAllScores,
+  fetchAllGameScores,
+  editScore,
+  deleteScore,
+  createScore 
+} from "../scores/scoresSlice";
+
+
 
 const SingleGame = () => {
   // put user ID in props????
   const userId = useSelector((state) => state.auth.me.id);
-  const {id} = useParams();
-const gameId = id
-
+  const { id } = useParams();
+  const gameId = id;
 
   const dispatch = useDispatch();
   const game = useSelector(selectSingleGame);
   const scores = useSelector(selectAllScores);
 
-  
   useEffect(() => {
     dispatch(fetchSingleGame(gameId));
     // dispatch(fetchAllScores())
-    dispatch(fetchAllGameScores(gameId))
+    dispatch(fetchAllGameScores(gameId));
   }, []);
 
-  
+  const handleAcceptRequest = (id) => {
+    console.log("IDDDD: ", id);
+    dispatch(editScore({ userId: id, gameId: game.id, accepted: true }));
+    dispatch(fetchSingleGame(gameId));
+    dispatch(fetchAllGameScores(gameId));
+  };
+  const handleDeclineRequest = (id) => {
+    console.log("DECLINE: ", id);
+    dispatch(deleteScore({ userId: id, gameId: game.id }));
+    dispatch(fetchSingleGame(gameId));
+    dispatch(fetchAllGameScores(gameId));
+  };
 
-
-  
-
-const handleAcceptRequest =(id)=>{
-    console.log("IDDDD: ", id)
-dispatch(editScore({userId: id, gameId: game.id, accepted: true}))
-dispatch(fetchSingleGame(gameId))
-dispatch(fetchAllGameScores(gameId))
-  }
-  const handleDeclineRequest =(id)=>{
-     console.log("DECLINE: ", id)
-dispatch(deleteScore({userId: id, gameId: game.id}))
- dispatch(fetchSingleGame(gameId))
- dispatch(fetchAllGameScores(gameId))
+  const handleAskJoin = ()=>{
+    dispatch(
+      createScore({ score: 0, accepted: false, gameId: gameId, userId: userId })
+    )
   }
 
   return (
@@ -47,73 +55,59 @@ dispatch(deleteScore({userId: id, gameId: game.id}))
       {game.owner ? <div>Owner: {game.owner.username}</div> : null}
 
       {/* Players and Score */}
-      {/* {game.users ? (
-        <div>
-          Players:{" "}
-          {game.users.map((user) => (
-            <div>
-              {user.username} {user.score.score}
-            </div>
-          ))}
-        </div>
-      ) : null} */}
 
-
-      {scores  ? (
+      {scores ? (
         <div>
           Playffers:{" "}
-          {scores.filter(score=> score.accepted)
-          .map((user) => (
-           <div> {user.user ?
+          {scores
+            .filter((score) => score.accepted)
+            .map((user) => (
+              <div>
+                {" "}
+                {user.user ? (
+                  <div>
+                    {user.user.username} {user.score}
+               {user.user.id !== userId ? <button onClick={() => handleDeclineRequest(user.user.id)}>
+                      Remove Player
+                    </button>:null}
+                  </div>
+                ) : null}
+              </div>
+            ))}
+        </div>
+      ) : null}
+
+      {/*IF GAME OWNER and Game NOT STARTED: Player Requests */}
+      {game.ownerId === userId && !game.started ? (
+        <div>Player Requests</div>
+      ) : null}
+      {game.ownerId === userId && !game.started ? (
+        <div>
+          {scores ? (
             <div>
-              {user.user.username} {user.score}
-              <button onClick={()=>handleDeclineRequest(user.user.id)}>Remove Player</button>
+              {scores
+                .filter((score) => !score.accepted)
+
+                .map((score) => (
+                  <div>
+                    {score.user.username}
+                    <button onClick={() => handleAcceptRequest(score.user.id)}>
+                      Accept
+                    </button>
+                    <button onClick={() => handleDeclineRequest(score.user.id)}>
+                      Decline
+                    </button>
+                  </div>
+                ))}
             </div>
-            : null}
-            </div>
-          ))}
+          ) : null}
         </div>
       ) : null}
 
 
-{/* PLayer Requests */}
-{game.ownerId === userId && !game.started ?<div>Player Requests</div> :null}
-
-{/* {game.ownerId === userId && !game.started ?
-<div>
-{game.users ? (
-  <div>
-    {game.scores.filter((score)=> !score.accepted)
-    
-    .map((score) => (
-      <div>
-        {score.user.username}
-        <button onClick={()=>handleAcceptRequest(score.user.id)}>Accept</button>
-        <button onClick={()=>handleDeclineRequest(score.user.id)}>Decline</button>
-      </div>
-    ))}
-  </div>
-) : null}
-</div>
-:null} */}
-
-
-{game.ownerId === userId && !game.started ?
-<div>
-{scores ? (
-  <div>
-    {scores.filter((score)=> !score.accepted)
-    
-    .map((score) => (
-      <div>
-        {score.user.username}
-        <button onClick={()=>handleAcceptRequest(score.user.id)}>Accept</button>
-        <button onClick={()=>handleDeclineRequest(score.user.id)}>Decline</button>
-      </div>
-    ))}
-  </div>
-) : null}
-</div>
+      {/* IF NOT GAME OWNER  and Game NOT STARTED: REQUEST TO JOIN*/}
+{game.ownerId !== userId && !game.started ?
+<button onClick={handleAskJoin} >Ask to join this game</button>
 :null}
 
     </div>
