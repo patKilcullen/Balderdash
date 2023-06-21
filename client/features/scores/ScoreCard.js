70f5d6752b295dd71f89cdda00bd954e0ceeccce
patKilcullen <patrickjkilcullen@gmail.com>
2023-06-21 01:00:07 -0500

    more player scorecard styling

--- client/features/scores/ScoreCard.js
+++ client/features/scores/ScoreCard.js
@@ -20,17 +20,16 @@ const ScoreCard = ({
   // const game = useSelector(selectSingleGame);
 
   // console.log("userId, userScore, game, SCORECARD: ", userId, userScore, game,)
-  
+
   return (
     <Card sx={{ boxShadow: "20", border: "2px solid black" }}>
       <Card
         sx={{
           padding: "10px",
           // color: "black",
-           backgroundColor: "#88ebe6",
-        // backgroundColor: "#e6e8dc",
-        // backgroundColor: "#558ABB" 
-    
+          backgroundColor: "#88ebe6",
+          // backgroundColor: "#e6e8dc",
+          // backgroundColor: "#558ABB"
         }}
       >
         {/* <Card id="scores-card"> */}
@@ -39,7 +38,9 @@ const ScoreCard = ({
           color="seconday"
           sx={{ boxShadow: "20", padding: "10px" }}
         >
-          <div style={{ width: "100%", display: "flex", flexDirection: "column", }}>
+          <div
+            style={{ width: "100%", display: "flex", flexDirection: "column" }}
+          >
             {/* <div
               style={{
                 display: "flex",
@@ -67,92 +68,236 @@ const ScoreCard = ({
               </div>
             </div> */}
 
-<Typography id = "title" color="secondary"sx={{fontWeight: "bold"}}>{game.name}</Typography>
-      <Divider  sx={{ border: "2px solid #571122", width: "95%", marginTop: "-15px", boxShadow: "1px 1px #558ABB"} } ></Divider>
-
-<Box sx={{display: "flex", width: "100%", marginTop: "20px",}}>
-    
-    <Card sx={{display: "flex", flexDirection: "column", width: "100%", border: "2px solid #571122", boxShadow: "2px 2px black" }}>
-    <Card sx={{ backgroundColor: "#e6e8dc",height: "100%", width: "100%"}}>
-    <Typography color="secondary"
-         sx={{ fontWeight: "bold", }}
-          variant="h4">  {userScore ? (
-            <div style={{width: "100%" ,display: "flex", flexDirection: "column", alignItems: "center", }}>
-            <div style={{fontSize:"30px", textDecoration: "underline"}}>Score:</div>
-            <div  style={{width: "100%", display: "flex", flexDirection: "row", gap: "1px", justifyContent: "center", alignItems: "flex-end", marginLeft: "20px"}}>
-           <div style={{color: "red", textDecoration: "none", }}>{userScore.score}</div>
-           <div style={{color: "black", fontSize: "20px"}}>pts</div>
-           
-            </div> 
-            
-            </div>
-            //  <div style={{textDecoration: "underline", width: "100%" ,display: "flex", flexDirection: "column", alignItems: "center"}}>Score: {userScore.score} </div>
-           ) : null}</Typography>
-    <Typography color="secondary"
-         sx={{ fontWeight: "bold", }}
-          variant="h4">  {game && game.rounds && game.roundsLeft? (
-             <div style={{width: "100%" ,display: "flex", flexDirection: "column", alignItems: "center"}}>
-             <div style={{fontSize:"30px", textDecoration: "underline"}}>Round:</div>
-            <div style={{color: "red", textDecoration: "none", }}>{game.rounds + 1 - game.roundsLeft }/{game.rounds} </div> 
-             
-             </div>
-           ) : null}</Typography>
-    </Card>
-    </Card>
-
-    <Card sx={{ width: "100%", border: "2px solid #571122",  boxShadow: "2px 2px black"}}>
-    <Card sx={{ backgroundColor: "#e6e8dc", height: "100%", width: "100%", display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <Typography color="secondary"
-         sx={{ fontWeight: "bold", textDecoration: "underline" ,width: "100%" ,display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "10px"}}
-          variant="h4">Players</Typography>
-        {scores ? (
-              <div >
-           
-                {scores
-                  .filter((score) => score.accepted && score.userId !== userId)
-                  .map((user) => (
-                    <div>
-                      {" "}
-                      {user.user ? (
-                        <div >
-                            <div style={{display: "flex", flexDirection: "row", gap: "10px"}}>
-                            <Typography color="secondary"
-         sx={{ fontWeight: "bold", fontSize: "30px", width: "100%" ,display: "flex", flexDirection: "column", alignItems: "center"}}
-          variant="h4">{user.user.username}: </Typography>
-
-          <Typography color="secondary"
-         sx={{ fontWeight: "bold", color: "red" ,width: "100%" }}
-          variant="h4">
-            <div style={{width: "100%", display: "flex", flexDirection: "row", gap: "1px", alignItems: "flex-end"}}>
-           <div> {user.score}</div>
-            <div style={{color: "black", fontSize: "20px"}}>pts</div>
-            </div>
+            <Typography
+              id="title"
+              color="secondary"
+              sx={{ fontWeight: "bold" }}
+            >
+              {game.name}
             </Typography>
-                          {/* {user.user.username}:  
-                          {user.score}pts */}
-                          </div>
-                          {/* Dont let non owner */}
-                          {user.user.id !== userId &&
-                          userId === game.ownerId &&
-                          game.started === false ? (
-                            <button
-                              onClick={() => handleDeclineRequest(user.user.id)}
-                            >
-                              Remove Player
-                            </button>
-                          ) : null}
+            <Divider
+              sx={{
+                border: "2px solid #571122",
+                width: "95%",
+                marginTop: "-15px",
+                boxShadow: "1px 1px #558ABB",
+              }}
+            ></Divider>
+
+            <Box sx={{ display: "flex", width: "100%", marginTop: "20px" }}>
+              <Card
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "100%",
+                  border: "2px solid #571122",
+                  boxShadow: "2px 2px black",
+                }}
+              >
+                <Card
+                  sx={{
+                    backgroundColor: "#e6e8dc",
+                    height: "100%",
+                    width: "100%",
+                  }}
+                >
+                  <Typography
+                    color="secondary"
+                    sx={{ fontWeight: "bold" }}
+                    variant="h4"
+                  >
+                    {" "}
+                    {userScore ? (
+                      <div
+                        style={{
+                          width: "100%",
+                          display: "flex",
+                          flexDirection: "column",
+                          alignItems: "center",
+                        }}
+                      >
+                        <div
+                          style={{
+                            fontSize: "30px",
+                            textDecoration: "underline",
+                          }}
+                        >
+                          Score:
                         </div>
-                      ) : null}
+                        <div
+                          style={{
+                            width: "100%",
+                            display: "flex",
+                            flexDirection: "row",
+                            gap: "1px",
+                            justifyContent: "center",
+                            alignItems: "flex-end",
+                            marginLeft: "20px",
+                          }}
+                        >
+                          <div style={{ color: "red", textDecoration: "none" }}>
+                            {userScore.score}
+                          </div>
+                          <div style={{ color: "black", fontSize: "20px" }}>
+                            pts
+                          </div>
+                        </div>
+                      </div>
+                    ) : //  <div style={{textDecoration: "underline", width: "100%" ,display: "flex", flexDirection: "column", alignItems: "center"}}>Score: {userScore.score} </div>
+                    null}
+                  </Typography>
+                  <Typography
+                    color="secondary"
+                    sx={{ fontWeight: "bold" }}
+                    variant="h4"
+                  >
+                    {" "}
+                    {game && game.rounds && game.roundsLeft ? (
+                      <div
+                        style={{
+                          width: "100%",
+                          display: "flex",
+                          flexDirection: "column",
+                          alignItems: "center",
+                        }}
+                      >
+                        <div
+                          style={{
+                            fontSize: "30px",
+                            textDecoration: "underline",
+                          }}
+                        >
+                          Round:
+                        </div>
+                        <div style={{ color: "red", textDecoration: "none" }}>
+                          {game.rounds + 1 - game.roundsLeft}/{game.rounds}{" "}
+                        </div>
+                      </div>
+                    ) : null}
+                  </Typography>
+                </Card>
+              </Card>
+
+              <Card
+                sx={{
+                  width: "100%",
+                  border: "2px solid #571122",
+                  boxShadow: "2px 2px black",
+                }}
+              >
+                <Card
+                  sx={{
+                    backgroundColor: "#e6e8dc",
+                    height: "100%",
+                    width: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    overflow: "scroll",
+                  }}
+                >
+                  <Typography
+                    color="secondary"
+                    sx={{
+                      fontWeight: "bold",
+                      textDecoration: "underline",
+                      width: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                      alignItems: "center",
+                      marginBottom: "10px",
+                    }}
+                    variant="h4"
+                  >
+                    Players
+                  </Typography>
+                  {scores ? (
+                    <div>
+                      {scores
+                        .filter(
+                          (score) => score.accepted && score.userId !== userId
+                        )
+                        .map((user) => (
+                          <div>
+                            {" "}
+                            {user.user ? (
+                              <div>
+                                <div
+                                  style={{
+                                    display: "flex",
+                                    flexDirection: "row",
+                                    gap: "1px",
+                                  }}
+                                >
+                                  <Typography
+                                    color="secondary"
+                                    sx={{
+                                      fontWeight: "bold",
+                                      fontSize: "25px",
+                                      width: "100%",
+                                      display: "flex",
+                                      flexDirection: "column",
+                                      alignItems: "center",
+                                    }}
+                                    variant="h4"
+                                  >
+                                    {user.user.username}:{" "}
+                                  </Typography>
+
+                                  <Typography
+                                    color="secondary"
+                                    sx={{
+                                      fontWeight: "bold",
+                                      color: "red",
+                                      width: "100%",
+                                    }}
+                                    variant="h4"
+                                  >
+                                    <div
+                                      style={{
+                                        fontSize: "30px",
+                                        width: "100%",
+                                        display: "flex",
+                                        flexDirection: "row",
+                                        gap: "1px",
+                                        alignItems: "flex-end",
+                                      }}
+                                    >
+                                      <div> {user.score}</div>
+                                      <div
+                                        style={{
+                                          color: "black",
+                                          fontSize: "20px",
+                                        }}
+                                      >
+                                        pts
+                                      </div>
+                                    </div>
+                                  </Typography>
+                                  {/* {user.user.username}:  
+                          {user.score}pts */}
+                                </div>
+                                {/* Dont let non owner */}
+                                {user.user.id !== userId &&
+                                userId === game.ownerId &&
+                                game.started === false ? (
+                                  <button
+                                    onClick={() =>
+                                      handleDeclineRequest(user.user.id)
+                                    }
+                                  >
+                                    Remove Player
+                                  </button>
+                                ) : null}
+                              </div>
+                            ) : null}
+                          </div>
+                        ))}
                     </div>
-                  ))}
-              </div>
-            ) : null}
-        </Card>
-        </Card>
-
-</Box>
-
-
+                  ) : null}
+                </Card>
+              </Card>
+            </Box>
 
             {/* <Typography>
               {userScore && userScore.user ? (
@@ -163,8 +308,6 @@ const ScoreCard = ({
             {userScore ? (
               <div> Your Score {userScore.score} </div>
             ) : null} */}
-
-
 
             {/* {scores ? (
               <div>
